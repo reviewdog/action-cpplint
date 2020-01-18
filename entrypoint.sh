267b84635e6d58e48c8b086934d0e6fe43613bef
srz_zumix <zumix.cpp@gmail.com>
2020-01-18 22:26:02 +0900

    Add filter input (#7)

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ cd "${GITHUB_WORKSPACE}" || exit
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-cpplint ${INPUT_FLAGS} ${INPUT_TARGETS} 2>&1 \
+cpplint ${INPUT_FLAGS} --filter="${INPUT_FILTER}" ${INPUT_TARGETS} 2>&1 \
     | reviewdog -efm="%f:%l: %m" -name="cpplint" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
